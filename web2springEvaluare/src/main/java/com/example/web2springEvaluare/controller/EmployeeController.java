b9046c50c64daa9af4fd563f7d83d040ab37043e
viitas96 <climav96@gmail.com>
2021-11-27 12:42:04 +0200

    added RestController EmployeeController and maping

---
+++ web2springEvaluare/src/main/java/com/example/web2springEvaluare/controller/EmployeeController.java
@@ -0,0 +1,9 @@
+package com.example.web2springEvaluare.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+}
