aa84197b6a7c69f91d9e858934de58e4e6e25435
viitas96 <climav96@gmail.com>
2021-11-27 12:09:18 +0200

    adding main controller and set table from database

---
+++ web2springEvaluare/src/main/java/com/example/web2springEvaluare/model/Employee.java
@@ -0,0 +1,57 @@
+package com.example.web2springEvaluare.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "idnp", nullable = false)
+    private int idnp;
+
+    @Column(name = "first_name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "salary", nullable = false)
+    private int salary;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getIdnp() {
+        return idnp;
+    }
+
+    public void setIdnp(int idnp) {
+        this.idnp = idnp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
