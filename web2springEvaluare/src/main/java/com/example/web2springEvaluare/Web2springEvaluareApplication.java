60d4376593c36cdafab4dccd7bf585c31d02572f
viitas96 <climav96@gmail.com>
2021-11-27 11:51:29 +0200

    Initial commit connceting data base

---
+++ web2springEvaluare/src/main/java/com/example/web2springEvaluare/Web2springEvaluareApplication.java
@@ -0,0 +1,13 @@
+package com.example.web2springEvaluare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2springEvaluareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2springEvaluareApplication.class, args);
+	}
+
+}
---
+++ web2springEvaluare/src/test/java/com/example/web2springEvaluare/Web2springEvaluareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2springEvaluare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2springEvaluareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
