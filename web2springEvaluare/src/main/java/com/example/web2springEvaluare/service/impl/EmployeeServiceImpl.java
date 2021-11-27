b0a3cc22d5e4e6e8fcdb0474ea58c0967b3a9e42
viitas96 <climav96@gmail.com>
2021-11-27 12:23:26 +0200

    create interface EmployeeServiceImpl

---
+++ web2springEvaluare/src/main/java/com/example/web2springEvaluare/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.web2springEvaluare.service.impl;
+
+import com.example.web2springEvaluare.model.Employee;
+import com.example.web2springEvaluare.service.EmployeeService;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService {
+    @Override
+    public Employee save(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return null;
+    }
+
+    @Override
+    public Employee get(long id) {
+        return null;
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee, long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+
+    }
+}
