a3cd968dbe0fb3fe3324b9d0fab9ae05ba316044
viitas96 <climav96@gmail.com>
2021-11-27 13:09:12 +0200

    created method create get

--- web2springEvaluare/src/main/java/com/example/web2springEvaluare/controller/EmployeeController.java
+++ web2springEvaluare/src/main/java/com/example/web2springEvaluare/controller/EmployeeController.java
@@ -4,10 +4,9 @@ import com.example.web2springEvaluare.model.Employee;
 import com.example.web2springEvaluare.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
@@ -23,4 +22,9 @@ public class EmployeeController {
     public ResponseEntity<Employee> create(@RequestBody Employee employee){
         return new ResponseEntity<Employee>(employeeService.save(employee), HttpStatus.OK);
     }
+
+    @GetMapping
+    public List<Employee> get(){ return employeeService.getAll(); }
+
+
 }
